26d60177666d2f6ea0f97493fefe49ac03384375
m.kikmunter <M.Kikmunter@gmail.com>
2018-07-08 19:39:25 +0100

    added Guard tests

---
+++ StoreProcedureRepository/StoredProcedureRepository.UnitTests/ServicesTests/GuardTests.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using FluentAssertions;
+using NUnit.Framework;
+using StoredProcedureRepository.Infrastructure.Services;
+
+namespace StoredProcedureRepository.UnitTests.ServicesTests
+{
+    [TestFixture]
+    public class GuardTests
+    {
+        [Test]
+        public void ThrowIfNull_NullArgument_ThrowsArgumentNullException()
+        {
+            //arrange
+            object input = null;
+
+            //act
+            Action result = () => Guard.ThrowIfNull(input);
+
+            //assert
+
+            result.Should().Throw<ArgumentNullException>();
+        }
+
+        [Test]
+        public void ThrowIfStringNullOrEmpty_EmptyString_ThrowsArgumentNullException()
+        {
+            //arrange
+            var input = string.Empty;
+
+            //act
+            Action result = () => Guard.ThrowIfStringNullOrEmpty(input);
+
+            //assert
+
+            result.Should().Throw<ArgumentNullException>();
+        }
+
+        [Test]
+        public void ThrowIfStringNullOrWhiteSpace_WhiteSpace_ThrowsArgumentNullException()
+        {
+            //arrange
+            var input = " ";
+
+            //act
+            Action result = () => Guard.ThrowIfStringNullOrWhiteSpace(input);
+
+            //assert
+
+            result.Should().Throw<ArgumentNullException>();
+        }
+
+        [Test]
+        public void ThrowIfAnyIsNullOrEmpty_OneStringIsEmpty_ThrowsArgumentException()
+        {
+            //arrange
+            var input = new[] { "Test", string.Empty };
+
+            //act
+            Action result = () => Guard.ThrowIfAnyIsNullOrEmpty(input);
+
+            //assert
+
+            result.Should().Throw<ArgumentException>();
+        }
+
+        [Test]
+        public void ThrowIfEmpty_EmptyList_ThrowsArgumentException()
+        {
+            //arrange
+            var input = new string[] { };
+
+            //act
+            Action result = () => Guard.ThrowIfEmpty(input);
+
+            //assert
+
+            result
+                .Should()
+                .Throw<ArgumentException>()
+                .WithMessage("entities cannot be empty.");
+        }
+    }
+}
